7f0915c9d1a706f1a62a4287cd932e1d88c0e048
“giovana-padua” <giogoncalvespadua13@gmail.com>
2026-05-25 09:09:54 -0300

    refactor: tabela de tcc com criado_em automatico

--- sistema-TCC/app/Models/Tcc.php
+++ sistema-TCC/app/Models/Tcc.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Tcc extends Model
 {
-    //
+    protected $fillable = ['id', 'orientador_id', 'tema', 'descricao', 'status', 'resultado_final', 'nota_final', 'created_at', 'updated_at'];
+    
+    protected $casts = [
+        'data' => 'datetime'
+    ];
 }
--- sistema-TCC/routes/web.php
+++ sistema-TCC/routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\TccController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('tccs', TccController::class);
+
+
