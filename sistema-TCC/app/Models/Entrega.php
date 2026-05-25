5b7bad0d076aa8ab64a08ceb6a973f99d2cb63b4
batmaans <gusttavor16@gmail.com>
2026-05-25 14:00:31 -0300

    “envio”

--- sistema-TCC/app/Models/Entrega.php
+++ sistema-TCC/app/Models/Entrega.php
@@ -5,10 +5,20 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 
 class Entrega extends Model
-{    
+{
     protected $table = 'entregas';
 
-    // infos tabela
     protected $fillable = ['tcc_id', 'titulo', 'descricao', 'prazo', 'status'];
 
+    // Uma entrega pertence a um TCC
+    public function tcc()
+    {
+        return $this->belongsTo(Tcc::class, 'tcc_id');
+    }
+
+    // Uma entrega tem muitos arquivos enviados
+    public function arquivos()
+    {
+        return $this->hasMany(ArquivoEntrega::class, 'entrega_id');
+    }
 }
