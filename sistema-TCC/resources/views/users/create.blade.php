111cfeb3157b59c6db20375c74c06a77759f4fb1
ana-manea <ana_manea@hotmail.com>
2026-05-31 22:19:34 -0300

    feat(auth): implementa perfil, autenticacao e controle de acesso

--- sistema-TCC/resources/views/users/create.blade.php
+++ sistema-TCC/resources/views/users/create.blade.php
@@ -6,20 +6,14 @@
     <div class="row">
         <div class="col-lg-8">
             <div class="card">
-                <div class="card-header">Cadastrar Usuário</div>
-
+                <div class="card-header">Novo Usuário</div>
                 <div class="card-body">
                     <form action="{{ route('users.store') }}" method="POST" class="vstack gap-3">
+
                         @include('users._form')
-
                         <div class="d-flex gap-2">
-                            <button type="submit" class="btn btn-primary">
-                                <i class="bi bi-check-circle"></i> Salvar
-                            </button>
-
-                            <a role="button" class="btn btn-outline-secondary" href="{{ route('users.index') }}">
-                                Cancelar
-                            </a>
+                            <button class="btn btn-primary">Salvar</button>
+                            <a href="{{ route('users.index') }}" class="btn btn-outline-secondary">Cancelar</a>
                         </div>
                     </form>
                 </div>
--- sistema-TCC/resources/views/users/show.blade.php
+++ sistema-TCC/resources/views/users/show.blade.php
@@ -5,53 +5,17 @@
 @section('content')
     @php
         $nomes = explode(' ', trim($user->name));
-
-        $iniciais = strtoupper(
-            substr($nomes[0], 0, 1) .
-            (count($nomes) > 1 ? substr(end($nomes), 0, 1) : '')
-        );
-
+        $iniciais = strtoupper(substr($nomes[0], 0, 1) . (count($nomes) > 1 ? substr(end($nomes), 0, 1) : ''));
         $corAvatar = $user->avatar ?? '#b20000';
     @endphp
-<div class="container-fluid py-4">
-    <div class="d-flex justify-content-between gap-4 align-items-center mb-3">
-        <div class="d-flex gap-4">
-            <a class="btn btn-outline-secondary btn-sm" href="{{ route('users.index') }}">
-                <i class="bi bi-arrow-left"></i> Ver todos os usuários
-            </a>
-            <h1 class="h3 mb-0 text-gray-800">Detalhes do Usuário</h1>
-        </div>
 
-        <div>
-            <a class="btn btn-outline-primary" href="{{ route('users.edit', $user) }}">
-                <i class="bi bi-pencil-square"></i> Editar
-            </a>
-        </div>
-    </div>
-        
-
-    <div class="card shadow-sm">
+    <div class="card">
         <div class="card-body">
-            <div class="mb-4">
-                <div class="avatar-user" @style(['background-color: ' . $corAvatar])>
-                    {{ $iniciais }}
-                </div>
-            </div>
-
-            <dl class="row">
-                <dt class="col-sm-3">Nome</dt>
-                <dd class="col-sm-9">{{ $user->name }}</dd>
-
-                <dt class="col-sm-3">E-mail</dt>
-                <dd class="col-sm-9">{{ $user->email }}</dd>
-
-                <dt class="col-sm-3">Função</dt>
-                <dd class="col-sm-9">{{ $user->funcao }}</dd>
-
-                <dt class="col-sm-3">Cor do avatar</dt>
-                <dd class="col-sm-9">{{ $corAvatar }}</dd>
-            </dl>
+            <div class="avatar-user mb-3" @style(['background-color: ' . $corAvatar])>{{ $iniciais }}</div>
+            <p><strong>Nome:</strong> {{ $user->name }}</p>
+            <p><strong>E-mail:</strong> {{ $user->email }}</p>
+            <p><strong>Função:</strong> {{ $user->funcao }}</p>
+            <a href="{{ route('users.index') }}" class="btn btn-outline-secondary">Voltar</a>
         </div>
     </div>
-</div>
 @endsection
